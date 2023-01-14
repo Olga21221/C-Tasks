fc912a1c18b95b0c1fa60fdeb0f0499f60d66d53
Olga <olgaolga21221@mail.ru>
2023-01-14 15:37:20 +0300

    add task 4

---
+++ Task004/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+for (int i = 1; i <= number; i++) {
+    Console.Write(i + " ");
+}
